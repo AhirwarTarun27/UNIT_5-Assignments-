1c66ca01a73f3a3bab7318bf92671b1aa9a47203
Tarun Ahirwar <codertarun328@gmail.com>
2022-02-21 19:34:02 +0530

    done

--- Coding/07_context_api/src/context/CartContentProvider.jsx
+++
@@ -1,7 +0,0 @@
-import { creatContext } from "react";
-
-const CartContext = createContext();
-
-export const CartContextProvider = ({ children }) => {
-  return <cartContext.Provider value={0}>{children}</cartContext.Provider>;
-};
--- Coding/New folder/store/actions.js
+++
@@ -1,5 +0,0 @@
-import { INC_COUNT } from "./actionTypes.js";
-
-export const incCount = (payload) => {
-  return { type: INC_COUNT, payload: payload };
-};
